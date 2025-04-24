942ec185a6de9b6f95fcfb2c838ea9baeeb2d69f
ByteWizard2 <shaheershahir7022@gmail.com>
2025-04-24 22:40:45 +0530

    folder structuring

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const Navbar = () => {
+  const [activeSection, setActiveSection] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  //check scroll and chage navbar background
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //smooth scroll function
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+  };
+
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "experience", label: "Experience" },
+    { id: "work", label: "Work" },
+    { id: "education", label: "Education" },
+  ];
+  return (
+    <nav
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md"
+          : "bg-transparent"
+      }`}
+    >
+      <div className="text-white py-5 flex justify-between items-center">
+        {/* {logo} */}
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Shaahir</span>
+          <span className="text-[#8245ec]">|</span>
+          <span className="text-white">P H</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+
+        {/* desktop menu */}
+        <ul className="hidden md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={`cursor-pointer hover:text-[#8245ec] ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }`}
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {/* {socila Media icons} */}
+        <div className="hidden md:flex space-x-4">
+          <a
+            href="https://github.com/ByteWizard2"
+            target="_bland"
+            rel="noopener noreferer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaGithub scale={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/shaahir08/"
+            target="_bland"
+            rel="noopener noreferer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaLinkedinIn scale={24} />
+          </a>
+        </div>
+
+        {/* {Mobile menu icon} */}
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className=" text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
+        </div>
+      </div>
+      {/* {Mobile Menu Item} */}
+      {isOpen && (
+        <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden">
+          <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+            {menuItems.map((items) => (
+              <li
+                key={items.id}
+                className={`cursor-pointer hover:text-white 
+                ${activeSection == items.id ? "text-[#8245ec]" : ""}`}
+              >
+                <button onClick={() => handleMenuItemClick(items.id)}>
+                  {items.label}
+                </button>
+              </li>
+            ))}
+            <div className="flex space-x-4">
+              <a
+                href="https://github.com/ByteWizard2"
+                target="_bland"
+                rel="noopener noreferer"
+                className="text-gray-300 hover:text-white"
+              >
+                <FaGithub scale={24} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/shaahir08/"
+                target="_bland"
+                rel="noopener noreferer"
+                className="text-gray-300 hover:text-white"
+              >
+                <FaLinkedinIn scale={24} />
+              </a>
+            </div>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
