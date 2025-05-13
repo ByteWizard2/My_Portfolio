730500bd07a8bcbbb70dd61d1f26e130f10f2a96
ByteWizard2 <shaheershahir7022@gmail.com>
2025-05-13 10:09:55 +0530

    bug fixing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,9 +47,9 @@ const Navbar = () => {
         {/* Logo */}
         <div className="text-lg font-semibold cursor-pointer">
           <span className="text-[#8245ec]">&lt;</span>
-          <span className="text-white">Tarun</span>
+          <span className="text-white">Shaahir </span>
           <span className="text-[#8245ec]">/</span>
-          <span className="text-white">Kaushik</span>
+          <span className="text-white">P H</span>
           <span className="text-[#8245ec]">&gt;</span>
         </div>
 
